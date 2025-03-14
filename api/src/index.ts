4e62a71c55ef71308290fc95f199b427a7661ce2
Raushan kumar <raushankumar8757825@gmail.com>
2025-03-14 13:17:16 +0530

    adding the dockerfile

--- api/src/index.ts
+++ api/src/index.ts
@@ -41,7 +41,7 @@ async function startServer() {
   await server.start();
   server.applyMiddleware({ app });
 
-  const PORT = process.env.PORT || 4000;
+  const PORT = process.env.PORT || 5000;
   httpServer.listen(PORT, () => {
     console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
     console.log(`📡 Subscriptions ready at ws://localhost:${PORT}${server.graphqlPath}`);
